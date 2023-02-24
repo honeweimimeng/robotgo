c7a410b98b30a83e09a3d3cc7574e469d8bb2064
honeweimimeng <2328108354@qq.com>
2023-02-24 14:53:23 +0800

    zjh

--- robotgo_win.go
+++ robotgo_win.go
@@ -81,6 +81,14 @@ func ScaleF(displayId ...int) (f float64) {
 	return f
 }
 
+func ScaleFSys() float64 {
+	f := float64(int(GetDPI(win.GetDesktopWindow()))) / 96.0
+	if f == 0.0 {
+		f = 1.0
+	}
+	return f
+}
+
 // GetMainDPI get the display dpi
 func GetMainDPI() int {
 	return int(GetDPI(GetHWND()))
